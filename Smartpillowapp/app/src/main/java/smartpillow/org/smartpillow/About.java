326525b017341126bc85ab82fb656c93cc90b91f
bokhamala <bokhamala@miners.utep.edu>
2016-11-02 18:49:42 -0600

    Update About.java

--- Smartpillowapp/app/src/main/java/smartpillow/org/smartpillow/About.java
+++ Smartpillowapp/app/src/main/java/smartpillow/org/smartpillow/About.java
@@ -1,19 +1,18 @@
 package smartpillow.org.smartpillow;
-
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 /**
  * Created by Daniel on 10/24/2016.
  */
-public class About extends Fragment {
+public class About extends Fragment 
+{
     private View myView;
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) 
+    {
         myView = inflater.inflate(R.layout.about, container, false);
-
         return myView;
     }
 }
